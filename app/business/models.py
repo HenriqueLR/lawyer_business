4809357672dcdf5bf73e4038981c65327659554a
Henrique Luz Rodrigues <henrique.lr89@gmail.com>
2022-01-16 11:34:41 -0300

    update app business django version 4.0

--- app/business/models.py
+++ app/business/models.py
@@ -1,8 +1,6 @@
-#coding: utf-8
 
 from django.db import models
 from django.contrib.auth.models import User
-
 
 
 class BusinessModel(models.Model):
@@ -11,7 +9,8 @@ class BusinessModel(models.Model):
     updated_at = models.DateTimeField(verbose_name=u'Atualizado em', auto_now=True, db_column='updated_at')
     created_at = models.DateTimeField(verbose_name=u'Criado em', auto_now_add=True, db_column='created_at')
     description = models.TextField(db_column='description', blank=True, null=True, verbose_name=u'Descricao')
-    user = models.OneToOneField(User, verbose_name='Usuario', related_name='business_user', db_column='user', unique=True)
+    user = models.OneToOneField(User, verbose_name='Usuario', related_name='business_user', db_column='user', 
+                                unique=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
