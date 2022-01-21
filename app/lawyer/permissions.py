077ce080e9fb5c8719fafddd4c5064e3a9b1a58e
Henrique Luz Rodrigues <henrique.lr89@gmail.com>
2022-01-20 23:13:12 -0300

    update app lawyer django version 4.0

--- app/lawyer/permissions.py
+++ app/lawyer/permissions.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.views.decorators.cache import never_cache
