17b12ca6414c21cb419caedb7f71b47de8271277
Henrique Luz Rodrigues <henrique.lr89@gmail.com>
2022-01-22 14:17:08 -0300

    update url django version 4.0

--- app/conf/urls.py
+++ app/conf/urls.py
@@ -1,29 +1,29 @@
-"""app URL Configuration
+"""hellow URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^test_setup/$', TemplateView.as_view(template_name="tests/setup.html",
+    path('admin/', admin.site.urls),
+    path('test_setup/', TemplateView.as_view(template_name="tests/setup.html",
                                             content_type="text/html"), name="test_setup"),
-    url(r'^', include('main.urls', namespace='main')),
-    url(r'^advogados/', include('lawyer.urls', namespace='lawyer')),
-    url(r'^empresas/', include('business.urls', namespace='business')),
-    url(r'^contas/', include('accounts.urls', namespace='accounts')),
+    path('', include(('main.urls', 'main'), namespace='main')),
+    path('advogados/', include(('lawyer.urls', 'lawyer'), namespace='lawyer')),
+    path('empresas/', include(('business.urls', 'business'), namespace='business')),
+    path('contas/', include(('accounts.urls', 'accounts'), namespace='accounts')),
 ]
