f112bea5d77d9ab805fe27f8effdc50f3d255d45
Henrique Luz Rodrigues <henrique.lr89@gmail.com>
2022-03-07 22:33:12 -0300

    update config environment

--- env/postgresql/configure-postgresql.sh
+++ env/postgresql/configure-postgresql.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -xe
 
+/etc/init.d/postgresql start
 sudo -u postgres psql --command "CREATE USER docker WITH SUPERUSER PASSWORD 'docker';"
 sudo -u postgres psql --command "CREATE DATABASE docker;"
 sudo -u postgres psql --command "GRANT ALL PRIVILEGES ON DATABASE docker to docker;"
-
 sudo echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/12/main/pg_hba.conf \
 && echo "listen_addresses='*'" >> /etc/postgresql/12/main/postgresql.conf \
 && /etc/init.d/postgresql restart
