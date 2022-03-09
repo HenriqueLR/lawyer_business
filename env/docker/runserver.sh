a0743d6abae8e7420064b0a00a82b64bc35b9bbd
Henrique Luz Rodrigues <henrique.lr89@gmail.com>
2022-03-08 22:33:12 -0300

    services scripts for docker

--- env/docker/runserver.sh
+++ env/docker/runserver.sh
@@ -3,16 +3,29 @@
 argc=$#
 argv=($@)
 
-Runserver(){
-    Remove;
-    docker run -d -i -t -v "$(pwd):/deploy/apps/lawyer_business" -v "pg-data:/var/lib/postgresql/12/main" -p 0.0.0.0:80:80 -p 0.0.0.0:4444:5432 --name "$image" "$image:$tag" ./env/docker/services.sh;
+bd(){
+    path_pg_data_docker="/var/lib/postgresql/12/main";
+    volume_local_pg_data="pg-data";
+    ip_port_bd=0.0.0.0:5432:5432;
+    service=bd;
+    remove;
+    docker run -d -i -t -v "$volume_local_pg_data:$path_pg_data_docker" -p $ip_port_bd --env-file ./.envdb --name "$image" "$image:$tag" ./env/docker/services.sh $service;
 }
 
-Remove(){
+app(){
+    path_app_docker="/deploy/apps/lawyer_business";
+    ip_port_app=0.0.0.0:80:80;
+    service=app;
+    remove;
+    docker run -d -i -t -v "$(pwd):$path_app_docker" -p $ip_port_app --link $parent:db --env-file ./.env --name "$image" "$image:$tag" ./env/docker/services.sh $service;
+}
+
+remove(){
     docker rm -f $image || true;
 }
 
 image=$1
 tag=$2
+parent=$4
 
-Runserver
+$3
--- env/docker/services.sh
+++ env/docker/services.sh
@@ -1,12 +1,45 @@
 #!/bin/bash -xe
 
-./app/manage.py collectstatic --noinput
+nginx_run(){
+	sudo cp ./env/nginx/default /etc/nginx/sites-enabled/default;
+	sudo /etc/init.d/nginx restart;		
+	return 0;
+}
 
-sudo cp ./env/nginx/default /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo /etc/init.d/postgresql restart
+postgresql_run(){
+	sudo /etc/init.d/postgresql restart;
+	/bin/bash
+	return 0;
+		
+}
 
-sudo cp ./env/supervisord/supervisord.conf /etc/supervisord.conf
-sudo supervisord -c /etc/supervisord.conf
-sudo supervisorctl restart all
+supervisord_run(){
+	supervisord -c /etc/supervisord/supervisord.conf
+	supervisorctl restart all;	
+	return 0;
+}
+
+django_run(){
+	./app/manage.py collectstatic --noinput;
+	find . -name "*.sqlite3" | xargs rm -f;
+	find . -name "*.pyc" | xargs rm -f;
+	./app/manage.py makemigrations --settings=conf.settings_production;
+	./app/manage.py migrate --settings=conf.settings_production;
+	return 0;
+}
+
+app(){
+	django_run;
+	supervisord_run;
+	nginx_run;
+	return 0;
+}
+
+bd(){
+	postgresql;
+	return 0;
+}
+
+$1
+
 /bin/bash
