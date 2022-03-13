2a66b3d85b84a0b1cc2510e934594b2244c5d30a
Henrique Luz Rodrigues <henrique.lr89@gmail.com>
2022-03-13 13:40:22 -0300

    remove return off functions

--- env/docker/services.sh
+++ env/docker/services.sh
@@ -3,20 +3,16 @@
 nginx_run(){
 	sudo cp ./env/nginx/default /etc/nginx/sites-enabled/default;
 	sudo /etc/init.d/nginx restart;		
-	return 0;
 }
 
 postgresql_run(){
 	sudo /etc/init.d/postgresql restart;
 	/bin/bash
-	return 0;
-		
 }
 
 supervisord_run(){
 	supervisord -c /etc/supervisord/supervisord.conf
 	supervisorctl restart all;	
-	return 0;
 }
 
 django_run(){
@@ -25,19 +21,16 @@ django_run(){
 	find . -name "*.pyc" | xargs rm -f;
 	./app/manage.py makemigrations --settings=conf.settings_production;
 	./app/manage.py migrate --settings=conf.settings_production;
-	return 0;
 }
 
 app(){
 	django_run;
 	supervisord_run;
 	nginx_run;
-	return 0;
 }
 
 bd(){
 	postgresql;
-	return 0;
 }
 
 $1
